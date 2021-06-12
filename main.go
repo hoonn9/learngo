50c0c5f03a58cf8179e92b31a6b919c3135886e0
HXXNS <hun9935@gmail.com>
2021-06-13 01:05:14 +0900

    Functions part Two

--- main.go
+++ main.go
@@ -1,34 +1,23 @@
-package main // 작성할 패키지 이름 (main 은 컴파일을 위함)
-// main packpage -> main func 를 찾고 시작
+package main
+
 import (
-	"fmt" // formatting
+	"fmt"
 	"strings"
 )
 
-// 인자의 타입을 스킵하면 뒤에있는 타입으로 선언 됨
-func multiply(a, b int) int {
-	return a * b
+// naked return
+// 리턴 타입에 변수를 바로 선언 (생성)
+// 함수가 끝나면 반환
+func lenAndUpper(name string) (length int, uppercase string) {
+	// defer 함수를 끝나고 실행
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
-// 여러 return 을 가질 수 있음
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
-}
 
 func main() {
-	// fmt.Println(multiply(2, 2))
-	
-	// 무조건 모든 return 을 받아야함
-	// 무시하려면 _ (컴파일러도 무시)
-	// totalLength, _ := lenAndUpper("nico")
-
 	totalLength, upperName := lenAndUpper("nico")
 	fmt.Println(totalLength, upperName)
-
-
-	repeatMe("hoon", "kozel", "blang", "good")
 }
