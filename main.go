036b2269c5547c9858f2f1c58f09564ce2d7b47d
HXXNS <hun9935@gmail.com>
2021-06-13 01:33:50 +0900

    Arrays and Slice

--- main.go
+++ main.go
@@ -6,13 +6,17 @@ import (
 
 
 func main() {
-	// a := 2
-	// b := &a
-	// a = 5
-	// fmt.Println(a, *b)
+	// limit item
+	names := [5]string{"hoon", "kozel", "blang"}
+	names[3] = "ddd"
+	names[4] = "agdsg"
 
-	a := 2
-	b := &a
-	*b = 20
-	fmt.Println(a)
+	// slice (not length)
+	names2 := []string{"hoon", "kozel", "blang"}
+	// names2[3] = "ddd" // Error ❌
+
+	// 기존 array 을 변경하지 않고 새로 만들어서 반환
+	names2 = append(names2, "good")
+
+	fmt.Println(names2)
 }
