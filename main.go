78282483864ba636f224327b417e3f1ed93a9a87
HXXNS <hun9935@gmail.com>
2021-06-13 01:28:50 +0900

    Pointers

--- main.go
+++ main.go
@@ -4,35 +4,15 @@ import (
 	"fmt"
 )
 
-func canIDrink(age int) bool {
-	switch age {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-
-	// 마치 if문처럼 사용가능
-	switch {
-	case age < 18:
-		return false
-	case age == 18:
-		return true
-	case age > 50:
-		return false
-	}
-
-	// 마치 if문처럼 사용가능
-	switch koreanAge := age + 2; koreanAge {
-		case  10:
-			return false
-		case  18:
-			return true
-	}
-	return false
-}
-
 
 func main() {
-	fmt.Println(canIDrink(16))
+	// a := 2
+	// b := &a
+	// a = 5
+	// fmt.Println(a, *b)
+
+	a := 2
+	b := &a
+	*b = 20
+	fmt.Println(a)
 }
