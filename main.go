7b7298d2c34af34e2586fb6201393773f5403f33
HXXNS <hun9935@gmail.com>
2021-06-15 22:22:19 +0900

    Channel

--- main.go
+++ main.go
@@ -5,22 +5,33 @@ import (
 	"time"
 )
 
+// Channel 은 Go routines 과 pipe와 같은 형태로 데이터를 주고 받는다.
 func main() {
-	// 함수 앞에 go만 써주면 병행성 실행
 
-	go sexyCount("hoon")
-	sexyCount("kozel")
+	// Channel 선언
+	// chan (주고 받은 정보 타입)
+	channel := make(chan string)
+	people := [5]string{"hoon", "kozel", "react", "typescript", "linux"}
 
-	// go routines 은 메인 함수가 끝나면 종료 됨 (기다려 주지 않음)
+	for _, person := range people {
+		go isSexy(person, channel)
+	}
+	// channel 에서 데이터 받기
+	// 받을 때 까지 기다림
+	// result := <- channel
 
-	// go sexyCount("kozel")
-	// go sexyCount("hoon")
+	// block operation 함수 멈춤 (<-)
+	// fmt.Println("Waiting messages...")
+	// fmt.Println("Received this message: ", <- channel)
+	// fmt.Println("Received this message: ", <- channel)
+
+	for i:=0; i<len(people); i++ {
+		fmt.Println(<- channel)
+	}
 }
 
-func sexyCount(person string) {
-	// top down
-	for i:=0; i<10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
-	}
+func isSexy(person string, channel chan string) {
+	time.Sleep(time.Second * 10)
+	// channel 에 데이터 보내기
+	channel <- person + " is sexy"
 }
