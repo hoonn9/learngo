255c8b472b9b7da7618f82191124b6bbf3ccf610
HXXNS <hun9935@gmail.com>
2021-06-15 21:18:45 +0900

    Dictionary Methods (Add, Update, Delete, Search)

--- main.go
+++ main.go
@@ -8,11 +8,27 @@ import (
 
 func main() {
 	dictionary := myDict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("first")
+
+	baseWord := "hello"
+	definition := "Greeting"
+
+	err := dictionary.Add(baseWord, definition)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
 	}
 	
+	hello, _ := dictionary.Search("hello")
+	fmt.Println(hello)
+
+	dictionary.Update(baseWord, "Second")
+	word, _ := dictionary.Search(baseWord)
+	fmt.Println(word)
+
+	baseWord2 := "hello2"
+
+	dictionary.Add(baseWord2, "world")
+
+	dictionary.Delete(baseWord2)
+	word2, _ := dictionary.Search(baseWord2)
+	fmt.Println(word2)
 }
--- myDict/myDict.go
+++ myDict/myDict.go
@@ -10,7 +10,12 @@ import "errors"
 // Dictionary type
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Not Found")
+var (
+	errNotFound = errors.New("Not Found")
+ errCantUpdate = errors.New("Cant update non-existing word")
+ errWordExists = errors.New("That word already exist")
+
+)
 
 // 에러 처리를 먼저
 // Search for a word
@@ -23,3 +28,46 @@ func (d Dictionary) Search(word string) (string, error) {
 	return "", errNotFound
 }
 
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	// if err == errNotFound {
+	// 	d[word] = def
+	// } else if err == nil {
+	// 	return errWordExists
+	// }
+
+	switch err {
+		case errNotFound:
+			d[word] = def
+		case nil:
+			return errWordExists
+	}
+
+	return nil
+}
+
+// Update a word
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+		case nil:
+			d[word] = definition
+		case errNotFound:
+			return errCantUpdate
+	}
+	return nil
+}
+
+
+// Delete a word
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+	switch err {
+		case nil:
+			delete(d, word)
+		case errNotFound:
+			return errNotFound
+	}
+	return nil
+}
