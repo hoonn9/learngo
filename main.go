1cc9c9e561a8913c4ff90e150de99c7be58bcc00
HXXNS <hun9935@gmail.com>
2021-06-21 21:40:04 +0900

    sequential job scrapper

--- main.go
+++ main.go
@@ -1,28 +1,39 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id	string
+	location string
+	title string
+	salary string
+	summary string
+}
+
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
+	var jobs []extractedJob
 	totalPages := getPages()
 	fmt.Println(totalPages)
 
 	for i:=0; i<totalPages; i++ {
-		getPage(i)
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
 	}
-}
 
-func getPage(page int) {
-	pageURL := baseURL + "&start=" +strconv.Itoa(page * 50)
-	fmt.Println("Requesting", pageURL)
+	writeJobs(jobs)
+	fmt.Println("Done. extracted.")
 }
 
 func getPages() int {
@@ -43,6 +54,61 @@ func getPages() int {
 	return pages
 }
 
+func writeJobs(jobs []extractedJob) {
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"ID", "Title", "Location", "Salary", "Summary"}
+
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range jobs {
+			jobSlice := []string{"https://kr.indeed.com/viewjob?jk=" + job.id, job.title, job.location, job.salary, job.summary}
+			jwErr := w.Write(jobSlice)
+			checkErr(jwErr)
+	}
+
+}
+
+
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
+	pageURL := baseURL + "&start=" +strconv.Itoa(page * 50)
+	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		job := extractJob(card)
+		jobs = append(jobs, job)
+	})
+
+	return jobs
+}
+
+
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("data-jk")
+	title := cleanString(card.Find(".title").Text())
+	location :=	cleanString(card.Find(".sjcl").Text())
+	salary :=	cleanString(card.Find(".salaryText").Text())
+	summary :=	cleanString(card.Find(".summary").Text())
+
+	return extractedJob{id: id, title: title, location: location, salary: salary, summary: summary}
+}
+
 func checkErr(err error) {
 	if err != nil {
 		log.Fatalln(err)
@@ -53,4 +119,9 @@ func checkCode(res *http.Response) {
 	if res.StatusCode != 200 {
 		log.Fatalln("Request failed with Status:", res.StatusCode)
 	}
+}
+
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
